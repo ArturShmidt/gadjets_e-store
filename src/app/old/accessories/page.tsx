c3146e599d40dee7f434e9374b5ebab5c68b6c12
Mykyta Musaiev <n.musaiev@gmail.com>
2025-09-13 15:45:36 +0300

    deleted unusable dirs

--- src/app/old/accessories/page.tsx
+++
@@ -1,5 +0,0 @@
-import Accessories from './accessories';
-
-export default function Page() {
-  return <Accessories />;
-}
--- src/app/old/tablets/page.tsx
+++
@@ -1,5 +0,0 @@
-import Tablets from './tablets';
-
-export default function Page() {
-  return <Tablets />;
-}
