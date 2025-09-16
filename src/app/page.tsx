e2d5e683753cda0f6d151a38a837b118769ebdfa
Artur Shmidt <artyrshmidt@gmail.com>
2025-09-16 12:43:06 +0300

    added props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,9 +20,9 @@ export default function Home() {
       </div>
       <div>
         <Carousel />
-        <ProductSlider title={'Brand new models'} />
+        <ProductSlider title="Brand new models" />
         <ShopByCategory />
-        <ProductSlider title={'Hot prices'} />
+        <ProductSlider title="Hot prices" />
         <AppleLoader className="text-white" />
         <AppleLoader
           size="lg"
