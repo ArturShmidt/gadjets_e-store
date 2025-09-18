2e15b9c5775818a541e224ee2907c98f6d1c73ef
Mykyta Musaiev <n.musaiev@gmail.com>
2025-09-18 11:20:17 +0300

    updated Request to NextRequest

--- src/app/api/products/[itemId]/route.ts
+++ src/app/api/products/[itemId]/route.ts
@@ -1,49 +1,16 @@
-import { NextResponse } from 'next/server';
-import path from 'path';
-import { promises as fs } from 'fs';
-import { ProductType as ProductDetails } from '@/types/CategoryType';
-import { Product as ProductSummary } from '@/types/product';
-
-const categoryToFileMap = {
-  phones: 'phones.json',
-  accessories: 'accessories.json',
-  tablets: 'tablets.json',
-};
-
-async function readJsonFile<T>(filename: string): Promise<T> {
-  const jsonDirectory = path.join(process.cwd(), 'data');
-  const fileContents = await fs.readFile(
-    path.join(jsonDirectory, filename),
-    'utf8',
-  );
-  return JSON.parse(fileContents);
-}
+import { NextResponse, NextRequest } from 'next/server';
+import { getProductById } from '@/lib/services/product.service';
 
 export async function GET(
-  request: Request,
-  { params }: { params: { itemId: string } },
+  request: NextRequest,
+  { params }: { params: Promise<{ itemId: string }> },
 ) {
-  const { itemId } = params;
+  const { itemId } = await params;
 
-  const allProducts = await readJsonFile<ProductSummary[]>('products.json');
-  const productSummary = allProducts.find((p) => p.itemId === itemId);
-
-  if (!productSummary) {
-    return new NextResponse('Product not found', { status: 404 });
-  }
-
-  const category = productSummary.category;
-  const detailedFileName = categoryToFileMap[category];
-
-  if (!detailedFileName) {
-    return new NextResponse('Product category not found', { status: 404 });
-  }
-
-  const detailedData = await readJsonFile<ProductDetails[]>(detailedFileName);
-  const productDetails = detailedData.find((item) => item.id === itemId);
+  const productDetails = await getProductById(itemId);
 
   if (!productDetails) {
-    return new NextResponse('Product details not found', { status: 404 });
+    return new NextResponse('Product not found', { status: 404 });
   }
 
   return NextResponse.json(productDetails);
