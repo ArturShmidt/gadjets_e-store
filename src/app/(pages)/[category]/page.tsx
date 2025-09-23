37709b8eefe40a759d6b39f3b63547acbea489eb
Mykyta Musaiev <n.musaiev@gmail.com>
2025-09-24 00:38:45 +0300

    dynamised titles and metadata FOREACH page on the site

--- src/app/(pages)/[category]/page.tsx
+++ src/app/(pages)/[category]/page.tsx
@@ -1,22 +1,33 @@
-// #region Imports
-
 import { Product as ProductSummary } from '@/types/product';
-
-// for component WTF
-
 import Catalog from '@/components/Products/Catalog';
-
 import { CategoryName } from '@/types/CategoryName';
-
 import { getValidCategories } from '@/lib/services/product.service';
 import { getProductsByCategory } from '@/lib/services/product.service';
 import CategoryHeader from '@/components/UI/CategoryHeader';
+import { Metadata } from 'next';
 
-// #endregion
+async function getCategoryDisplayName(slug: string): Promise<string> {
+  const nameMap: { [key: string]: string } = {
+    phones: 'Phones',
+    tablets: 'Tablets',
+    accessories: 'Accessories',
+  };
+  return nameMap[slug] || slug.charAt(0).toUpperCase() + slug.slice(1);
+}
 
-/**
- * Валідація: Створюємо список всіх валідних URL-параметрів.
- */
+export async function generateMetadata({
+  params,
+}: {
+  params: { category: CategoryName };
+}): Promise<Metadata> {
+  const categoryName = await getCategoryDisplayName(params.category);
+
+  return {
+    title: `Shop ${categoryName} Online - Catalog`,
+    description: `Explore our wide selection of ${categoryName.toLowerCase()} at the Nice Gadgets store. Find the best deals and latest models.`,
+  };
+}
+
 export async function generateStaticParams() {
   const categories = await getValidCategories();
 
@@ -34,12 +45,33 @@ export default async function CategoryPage({
 }) {
   const { category } = params;
 
-  // Викликаємо функцію з сервісу
   const initialProducts: ProductSummary[] =
     await getProductsByCategory(category);
 
+  const jsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'BreadcrumbList',
+    'itemListElement': [
+      {
+        '@type': 'ListItem',
+        'position': 1,
+        'name': 'Home',
+        'item': 'https://gadgets-e-store.vercel.app/',
+      },
+      {
+        '@type': 'ListItem',
+        'position': 2,
+        'name': category,
+      },
+    ],
+  };
+
   return (
     <div className="dark:bg-dark-theme-bg px-4 sm:px-6 lg:px-8 pt-6 pb-16 lg:pb-20">
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
       <CategoryHeader
         categoryName={category}
         total={initialProducts.length}
