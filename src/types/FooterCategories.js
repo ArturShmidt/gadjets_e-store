a748275add755c8d837a058f647bf4e5a3ba45a6
Artur Shmidt <artyrshmidt@gmail.com>
2025-09-26 09:03:23 +0300

    change git url

--- src/types/FooterCategories.js
+++ src/types/FooterCategories.js
@@ -1,5 +1,5 @@
 export const FooterCategories = [
-  { href: 'https://github.com/ArturShmidt/gadjets_e-store', label: 'Github' },
+  { href: 'https://github.com/Teaam-3/gadjets_e-store', label: 'Github' },
   { href: '/contacts', label: 'Contacts' },
   { href: '/rights', label: 'Rights' },
 ];
